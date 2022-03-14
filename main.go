ebab43b0b0a7808d02526d30f5924ce7079e88b0
hindatik <hind.atik@ynov.com>
2022-03-14 15:03:26 +0100

    perso

--- main.go
+++ main.go
@@ -32,7 +32,44 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		url := "https://anapioficeandfire.com/api/characters/583"
+		url := "https://anapioficeandfire.com/api/characters/"
+
+		httpClient := http.Client{
+			Timeout: time.Second * 8,
+		}
+
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		res, GetErr := httpClient.Do(req)
+		if GetErr != nil {
+			log.Fatal(GetErr)
+		}
+
+		req.Header.Set("User-Agent", "seb go tuto v2")
+
+		if res.Body != nil {
+			defer res.Body.Close()
+		}
+
+		body, readErr := ioutil.ReadAll(res.Body)
+		if readErr != nil {
+			log.Fatal(readErr)
+		}
+
+		response := People{}
+		jsonErr := json.Unmarshal(body, &response)
+		if jsonErr != nil {
+			log.Fatal(jsonErr)
+		}
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.Execute(w, response)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		url := "https://anapioficeandfire.com/api/characters/"
 
 		httpClient := http.Client{
 			Timeout: time.Second * 8,
